14a70db774c6027f92e2d563d67e8557f47a339c
michael <michale.ns32@gmail.com>
2021-12-29 21:16:58 +0200

    step 1

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+};
